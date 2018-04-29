3daa91abe9712861d741d1112abf0a0efb032085
AkshayVaswani <34140914+AkshayVaswani@users.noreply.github.com>
2018-04-29 10:38:53 -0400

    pc home 4-28

---
+++ app/src/main/java/com/example/a10012032/dreamapplication/UserAuth/Login.java
@@ -0,0 +1,21 @@
+package com.example.a10012032.dreamapplication.UserAuth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.a10012032.dreamapplication.MainActivity;
+import com.example.a10012032.dreamapplication.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
